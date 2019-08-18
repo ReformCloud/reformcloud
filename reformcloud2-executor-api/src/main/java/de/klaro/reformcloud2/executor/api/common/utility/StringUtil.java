12c923d03aff4f2edbc99944af084cf91b3c98fe
Pasqual K <pasqual.koschmieder@gmail.com>
2019-08-18 17:45:32 +0200

    Added header

---
+++ reformcloud2-executor-api/src/main/java/de/klaro/reformcloud2/executor/api/common/utility/StringUtil.java
@@ -0,0 +1,16 @@
+package de.klaro.reformcloud2.executor.api.common.utility;
+
+public final class StringUtil {
+
+    public static void sendHeader() {
+        System.out.println(
+                "   __       __                        ___ _                 _ ____  \n" +
+                "  /__\\ ___ / _| ___  _ __ _ __ ___   / __\\ | ___  _   _  __| |___ \\ \n" +
+                " / \\/// _ \\ |_ / _ \\| '__| '_ ` _ \\ / /  | |/ _ \\| | | |/ _` | __) |\n" +
+                "/ _  \\  __/  _| (_) | |  | | | | | / /___| | (_) | |_| | (_| |/ __/ \n" +
+                "\\/ \\_/\\___|_|  \\___/|_|  |_| |_| |_\\____/|_|\\___/ \\__,_|\\__,_|_____|\n" +
+                "\n" +
+                "                  Not just a cloud system, but an experience.\n"
+        );
+    }
+}
--- reformcloud2-executor/src/main/java/de/klaro/reformcloud2/executor/controller/ControllerLauncher.java
+++ reformcloud2-executor/src/main/java/de/klaro/reformcloud2/executor/controller/ControllerLauncher.java
@@ -1,10 +1,12 @@
 package de.klaro.reformcloud2.executor.controller;
 
 import de.klaro.reformcloud2.executor.api.common.dependency.DependencyLoader;
+import de.klaro.reformcloud2.executor.api.common.utility.StringUtil;
 
 public final class ControllerLauncher {
 
     public static synchronized void main(String[] args) {
+        StringUtil.sendHeader();
         DependencyLoader.doLoad();
     }
 }
