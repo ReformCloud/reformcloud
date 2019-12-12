d176fe4c66fca91ac84ef90f8c96f48cfa2a03ec
_Klaro <pasqual.koschmieder@gmail.com>
2019-12-12 20:50:52 +0100

    Small fix

--- reformcloud2-external/reformcloud2-default-application-permissions/src/main/java/systems/reformcloud/reformcloud2/permissions/application/ReformCloudApplication.java
+++ reformcloud2-external/reformcloud2-default-application-permissions/src/main/java/systems/reformcloud/reformcloud2/permissions/application/ReformCloudApplication.java
@@ -1,7 +1,11 @@
 package systems.reformcloud.reformcloud2.permissions.application;
 
+import systems.reformcloud.reformcloud2.executor.api.ExecutorType;
+import systems.reformcloud.reformcloud2.executor.api.common.ExecutorAPI;
 import systems.reformcloud.reformcloud2.executor.api.common.application.api.Application;
+import systems.reformcloud.reformcloud2.executor.api.common.commands.manager.CommandManager;
 import systems.reformcloud.reformcloud2.executor.controller.ControllerExecutor;
+import systems.reformcloud.reformcloud2.executor.node.NodeExecutor;
 import systems.reformcloud.reformcloud2.permissions.PermissionAPI;
 import systems.reformcloud.reformcloud2.permissions.application.command.CommandPerms;
 import systems.reformcloud.reformcloud2.permissions.packets.PacketHelper;
@@ -13,11 +17,17 @@ public class ReformCloudApplication extends Application {
         PacketHelper.addControllerPackets();
         PermissionAPI.handshake();
 
-        ControllerExecutor.getInstance().getCommandManager().register(new CommandPerms());
+        getCommandManager().register(new CommandPerms());
     }
 
     @Override
     public void onUninstall() {
         PacketHelper.unregisterControllerPackets();
+    }
+
+    private CommandManager getCommandManager() {
+        return ExecutorAPI.getInstance().getType().equals(ExecutorType.CONTROLLER)
+                ? ControllerExecutor.getInstance().getCommandManager()
+                : NodeExecutor.getInstance().getCommandManager();
     }
 }
