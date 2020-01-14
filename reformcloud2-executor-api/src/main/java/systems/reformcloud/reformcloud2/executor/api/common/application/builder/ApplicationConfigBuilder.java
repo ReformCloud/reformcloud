ead71603502dbb7d7f2e56acad658195fe41933f
_Klaro <pasqual.koschmieder@gmail.com>
2020-01-14 21:22:52 +0100

    Changed version string to 2.0.2

--- reformcloud2-executor-api/src/main/java/systems/reformcloud/reformcloud2/executor/api/common/application/builder/ApplicationConfigBuilder.java
+++ reformcloud2-executor-api/src/main/java/systems/reformcloud/reformcloud2/executor/api/common/application/builder/ApplicationConfigBuilder.java
@@ -36,7 +36,7 @@ public final class ApplicationConfigBuilder {
 
     private String website = "https://reformcloud.systems";
 
-    private String implementedVersion = "2.0.2-SNAPSHOT";
+    private String implementedVersion = "2.0.2";
 
     private final List<Dependency> dependencies = new ArrayList<>();
 
