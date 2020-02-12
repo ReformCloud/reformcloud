e8e261e747119f0621bb7b635c047bf9dcde0ab6
Pasqual K <pasqual.koschmieder@gmail.com>
2020-02-12 20:25:30 +0100

    Added missing documentation to message api methods

--- reformcloud2-executor-api/src/main/java/systems/reformcloud/reformcloud2/executor/api/common/api/messaging/MessageAsyncAPI.java
+++ reformcloud2-executor-api/src/main/java/systems/reformcloud/reformcloud2/executor/api/common/api/messaging/MessageAsyncAPI.java
@@ -8,16 +8,39 @@ import javax.annotation.Nonnull;
 
 public interface MessageAsyncAPI {
 
+    /**
+     * Sends a channel message to the specified sender
+     *
+     * @param receiver          The receiver of the channel message
+     * @param jsonConfiguration The content which should get sent
+     * @return A task which will get completed when the message got sent to all senders
+     */
     @Nonnull
     default Task<Void> sendChannelMessageAsync(@Nonnull String receiver, @Nonnull JsonConfiguration jsonConfiguration) {
         return sendChannelMessageAsync(jsonConfiguration, receiver);
     }
 
+    /**
+     * Sends a channel message to a bunch of receivers
+     *
+     * @param jsonConfiguration The content which should get sent
+     * @param receivers         The receivers of the channel message
+     * @return A task which will get completed when the message got sent to all senders
+     */
     @Nonnull
     default Task<Void> sendChannelMessageAsync(@Nonnull JsonConfiguration jsonConfiguration, @Nonnull String... receivers) {
         return sendChannelMessageAsync(jsonConfiguration, ErrorReportHandling.NOTHING, receivers);
     }
 
+    /**
+     * Sends a channel message to a bunch of receivers and specifies the error handling if a specified
+     * receiver is not available in the network
+     *
+     * @param jsonConfiguration   The content which should get sent
+     * @param errorReportHandling The handling type of errors if a channel is not available in the network
+     * @param receivers           The receivers of the channel message
+     * @return A task which will get completed when the message got sent to all senders
+     */
     @Nonnull
     Task<Void> sendChannelMessageAsync(@Nonnull JsonConfiguration jsonConfiguration, @Nonnull ErrorReportHandling errorReportHandling, @Nonnull String... receivers);
 }
--- reformcloud2-executor-api/src/main/java/systems/reformcloud/reformcloud2/executor/api/common/api/messaging/MessageSyncAPI.java
+++ reformcloud2-executor-api/src/main/java/systems/reformcloud/reformcloud2/executor/api/common/api/messaging/MessageSyncAPI.java
@@ -7,13 +7,33 @@ import javax.annotation.Nonnull;
 
 public interface MessageSyncAPI {
 
+    /**
+     * Sends a channel message to the specified sender
+     *
+     * @param receiver          The receiver of the channel message
+     * @param jsonConfiguration The content which should get sent
+     */
     default void sendChannelMessageSync(@Nonnull String receiver, @Nonnull JsonConfiguration jsonConfiguration) {
         sendChannelMessageSync(jsonConfiguration, receiver);
     }
 
+    /**
+     * Sends a channel message to a bunch of receivers
+     *
+     * @param jsonConfiguration The content which should get sent
+     * @param receivers         The receivers of the channel message
+     */
     default void sendChannelMessageSync(@Nonnull JsonConfiguration jsonConfiguration, @Nonnull String... receivers) {
         sendChannelMessageSync(jsonConfiguration, ErrorReportHandling.NOTHING, receivers);
     }
 
+    /**
+     * Sends a channel message to a bunch of receivers and specifies the error handling if a specified
+     * receiver is not available in the network
+     *
+     * @param jsonConfiguration   The content which should get sent
+     * @param errorReportHandling The handling type of errors if a channel is not available in the network
+     * @param receivers           The receivers of the channel message
+     */
     void sendChannelMessageSync(@Nonnull JsonConfiguration jsonConfiguration, @Nonnull ErrorReportHandling errorReportHandling, @Nonnull String... receivers);
 }
