f60ad31b2ca9cfe6697adb4e13f2422783eece36
Pasqual K <pasqual.koschmieder@gmail.com>
2020-05-12 19:42:55 +0200

    Ignore inclusions with process filter

--- reformcloud2-executor-api/src/main/java/systems/reformcloud/reformcloud2/executor/api/common/process/running/matcher/PreparedProcessFilter.java
+++ reformcloud2-executor-api/src/main/java/systems/reformcloud/reformcloud2/executor/api/common/process/running/matcher/PreparedProcessFilter.java
@@ -28,7 +28,6 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import systems.reformcloud.reformcloud2.executor.api.common.process.ProcessInformation;
 import systems.reformcloud.reformcloud2.executor.api.common.process.api.ProcessConfiguration;
-import systems.reformcloud.reformcloud2.executor.api.common.process.api.ProcessInclusion;
 
 import java.util.Collection;
 
@@ -63,24 +62,6 @@ public final class PreparedProcessFilter {
                 || configuration.getTemplate().getName().equals(information.getProcessDetail().getTemplate().getName()))
                 && (configuration.getMaxMemory() == null || configuration.getMaxMemory().equals(information.getProcessDetail().getMaxMemory()))
                 && configuration.getExtra().toPrettyString().equals(information.getExtra().toPrettyString())
-                && (configuration.getDisplayName() == null || information.getProcessDetail().getDisplayName().equals(configuration.getDisplayName()))
-                && matchesAllInclusions(information, configuration);
-    }
-
-    private static boolean matchesAllInclusions(@NotNull ProcessInformation processInformation,
-                                                @NotNull ProcessConfiguration configuration) {
-        if (processInformation.getPreInclusions().size() != configuration.getInclusions().size()) {
-            return false;
-        }
-
-        for (ProcessInclusion inclusion : configuration.getInclusions()) {
-            if (processInformation.getPreInclusions().stream().noneMatch(
-                    e -> e.getName().equals(inclusion.getName()) && e.getUrl().equals(inclusion.getUrl())
-            )) {
-                return false;
-            }
-        }
-
-        return true;
+                && (configuration.getDisplayName() == null || information.getProcessDetail().getDisplayName().equals(configuration.getDisplayName()));
     }
 }
